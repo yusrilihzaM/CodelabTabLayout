65f8e5bab7437e3433dadd1c55e7199ffae54434
yusrilihzaM <mohyusrilihzamaulana@gmail.com>
2021-03-16 21:10:09 +0700

    1 Fragment 3 Judul

--- app/src/main/java/com/yusril/codelabtablayout/HomeFragment.kt
+++ app/src/main/java/com/yusril/codelabtablayout/HomeFragment.kt
@@ -5,10 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 
 class HomeFragment : Fragment() {
 
+
+    companion object{
+        private const val ARG_SECTION_NUMBER="Section Number"
+
+        @JvmStatic
+        fun newInstance(index:Int)=  HomeFragment().apply {
+            arguments = Bundle().apply {
+                putInt(ARG_SECTION_NUMBER, index)
+            }
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,5 +29,11 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tvLabel: TextView = view.findViewById(R.id.section_label)
 
+        val index = arguments?.getInt(ARG_SECTION_NUMBER, 0)
+        tvLabel.text = getString(R.string.content_tab_text, index)// ambil judul
+    }
 }
--- app/src/main/java/com/yusril/codelabtablayout/viewpager/SectionsPagerAdapter.kt
+++ app/src/main/java/com/yusril/codelabtablayout/viewpager/SectionsPagerAdapter.kt
@@ -9,16 +9,17 @@ import com.yusril.codelabtablayout.ProfileFragment
 class SectionsPagerAdapter (activity: AppCompatActivity) : FragmentStateAdapter(activity){
     override fun getItemCount(): Int {
         // sesuai dengan banyak fragment tab
-        return 2
+        return 3
     }
 
     override fun createFragment(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when (position) {
-            0 -> fragment = HomeFragment()
-            1 -> fragment = ProfileFragment()
-        }
-        return fragment as Fragment
+//        var fragment: Fragment? = null
+//        when (position) {
+//            0 -> fragment = HomeFragment()
+//            1 -> fragment = ProfileFragment()
+//        }
+//        return fragment as Fragment
+        return HomeFragment.newInstance(position + 1)
     }
 
 }
