6b2d3966673b3ded3eaaa5a10c38d6cae7fc9292
yusrilihzaM <mohyusrilihzamaulana@gmail.com>
2021-03-16 21:01:59 +0700

    Tab Layout View Pager

--- app/src/main/java/com/yusril/codelabtablayout/MainActivity.kt
+++ app/src/main/java/com/yusril/codelabtablayout/MainActivity.kt
@@ -2,10 +2,43 @@ package com.yusril.codelabtablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.yusril.codelabtablayout.viewpager.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        @StringRes
+        private  val TAB_TITLES= intArrayOf(
+                R.string.tab_text_1,
+                R.string.tab_text_2
+        )
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // viewpager
+        val sectionsPagerAdapter=SectionsPagerAdapter(this)
+        // inisialisasi view pager
+        val viewPager:ViewPager2=findViewById(R.id.view_pager)
+        viewPager.adapter=sectionsPagerAdapter// set adapter view pager
+
+        // inisialisasi tab
+        val tabs:TabLayout=findViewById(R.id.tabs)
+        // masukan tab dan view pager
+        /*
+        * Dengan menerapkan TabLayoutMediator, maka Fragment yang tampil pada ViewPager2
+        * akan sesuai dengan posisi yang dipilih pada tab. Selain itu, di sini Anda juga
+        * menentukan judul dari masing-masing Tab dengan menggunakan
+        * TAB_TITLE yang diambil sesuai dengan urutan posisinya.
+        * */
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+        supportActionBar?.elevation=0f
     }
 }
